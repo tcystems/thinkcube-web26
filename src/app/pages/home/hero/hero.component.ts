400a485bcf464a0967d394d836bbbe5a89ecb2bc
Keheliya-Sathsara <keheliyasm@gmail.com>
2026-07-15 00:40:25 +0530

    changes

--- src/app/pages/home/hero/hero.component.ts
+++ src/app/pages/home/hero/hero.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Inject, PLATFORM_ID, ViewChild } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCircleCheck } from '@fortawesome/free-solid-svg-icons';
 
@@ -8,6 +9,21 @@ import { faCircleCheck } from '@fortawesome/free-solid-svg-icons';
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss',
 })
-export class HeroComponent {
+export class HeroComponent implements AfterViewInit {
   faCircleCheck = faCircleCheck;
+
+  @ViewChild('heroVideo') heroVideo?: ElementRef<HTMLVideoElement>;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngAfterViewInit(): void {
+    if (!isPlatformBrowser(this.platformId) || !this.heroVideo) {
+      return;
+    }
+    // SSR hydration can leave the `muted` IDL property false even though the
+    // attribute is present, which silently blocks autoplay. Set it explicitly.
+    const video = this.heroVideo.nativeElement;
+    video.muted = true;
+    video.play().catch(() => {});
+  }
 }
